a355c49384641079eb113c50855655cf2b93c0f3
nawad1 <nawad1@chicagobooth.edu>
2024-07-17 03:09:25 +0000

    more progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
   get("/directors", {:controller => "directors", :action =>"index"})
   get("/directors/:the_id", {:controller => "directors", :action =>"show"})
 
-  get("/movie", {:controller => "movies", :action =>"index"})
-  get("/movie/:the_id", {:controller => "movies", :action =>"show"})
+  get("/movies", {:controller => "movies", :action =>"index"})
+  get("/movies/:the_id", {:controller => "movies", :action =>"show"})
+
+  get("/actors", {:controller => "actors", :action =>"index"})
+  get("/actors/:the_id", {:controller => "actors", :action =>"show"})
 end
